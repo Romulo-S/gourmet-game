cd580982304f794221e7413a5311d63dd8809a24
romulo <romulosorato@hotmail.com>
2020-03-21 02:18:18 -0300

    bug correction

--- src/com/objective/Main.java
+++ src/com/objective/Main.java
@@ -15,7 +15,9 @@ public class Main {
         JOptionPane.showMessageDialog(null, "Think in a food you like", "", JOptionPane.INFORMATION_MESSAGE);
 
         boolean play = true;
+        boolean lose = false;
         do {
+            lose = false;
             // using for-each loop for iteration over Map.entrySet()
             for (Map.Entry<Category, List<Food>> map : entry.entrySet()) {
                 Category key = map.getKey();
@@ -26,44 +28,55 @@ public class Main {
                         option = JOptionPane.showConfirmDialog(null, "Is  " + food.getName() + " your food?", "Gourmet Game", JOptionPane.YES_NO_CANCEL_OPTION);
                         if (option == JOptionPane.OK_OPTION) {
                             JOptionPane.showMessageDialog(null, "I got it!", "", JOptionPane.INFORMATION_MESSAGE);
-                            return;
+                            option = JOptionPane.showConfirmDialog(null, "Continue?", "Gourmet Game", JOptionPane.YES_NO_OPTION);
+                            if (option == JOptionPane.OK_OPTION) {
+                                lose = true;
+                            } else if (option == JOptionPane.NO_OPTION || option == JOptionPane.DEFAULT_OPTION) {
+                                return;
+                            }
                         } else if (option == JOptionPane.CANCEL_OPTION || option == JOptionPane.DEFAULT_OPTION) {
                             return;
                         }
                     }
-                    String answer = JOptionPane.showInputDialog("Ok you win, what is your food?");
+                    if (lose != true) {
+                        String answer = JOptionPane.showInputDialog("Ok you win, what is your food?");
 
-                    Food newFood = new Food(answer);
-                    value.add(newFood);
-                    entry.put(key, value);
-                    option = JOptionPane.showConfirmDialog(null, "Continue?", "Gourmet Game", JOptionPane.YES_NO_OPTION);
-                    if (option == JOptionPane.OK_OPTION) {
-                        break;
-                    } else if (option == JOptionPane.NO_OPTION || option == JOptionPane.DEFAULT_OPTION) {
-                        return;
+                        Food newFood = new Food(answer);
+                        value.add(newFood);
+                        entry.put(key, value);
+                        option = JOptionPane.showConfirmDialog(null, "Continue?", "Gourmet Game", JOptionPane.YES_NO_OPTION);
+                        if (option == JOptionPane.OK_OPTION) {
+                            continue;
+                        } else if (option == JOptionPane.NO_OPTION || option == JOptionPane.DEFAULT_OPTION) {
+                            return;
+                        }
                     }
                 } else if (option == JOptionPane.CANCEL_OPTION || option == JOptionPane.DEFAULT_OPTION) {
                     return;
                 }
+
+            }
+            if (lose != true) {
+
+                /**Add new category**/
+                String answer = JOptionPane.showInputDialog("Ok you win, how do you describe the category of your food??");
+                Category newCategory = new Category(answer);
+                /**Add new food**/
+                answer = JOptionPane.showInputDialog(" what is the name of your food?");
+                Food newFood = new Food(answer);
+
+                List<Food> foods = new ArrayList<>();
+                foods.add(newFood);
+                entry.put(newCategory, foods);
+
+                int option = JOptionPane.showConfirmDialog(null, "Continue?", "Gourmet Game", JOptionPane.YES_NO_OPTION);
+                if (option == JOptionPane.OK_OPTION) {
+                    continue;
+                } else if (option == JOptionPane.NO_OPTION || option == JOptionPane.DEFAULT_OPTION) {
+                    return;
+                }
             }
 
-            /**Add new category**/
-            String answer = JOptionPane.showInputDialog("Ok you win, how do you describe the category of your food??");
-            Category newCategory = new Category(answer);
-            /**Add new food**/
-            answer = JOptionPane.showInputDialog(" what is the name of your food?");
-            Food newFood = new Food(answer);
-
-            List<Food> foods = new ArrayList<>();
-            foods.add(newFood);
-            entry.put(newCategory, foods);
-
-            int option = JOptionPane.showConfirmDialog(null, "Continue?", "Gourmet Game", JOptionPane.YES_NO_OPTION);
-            if (option == JOptionPane.OK_OPTION) {
-                continue;
-            } else if (option == JOptionPane.NO_OPTION || option == JOptionPane.DEFAULT_OPTION) {
-                return;
-            }
 
         } while (play);
 
